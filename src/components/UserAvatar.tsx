b554f9be7c7a1ed76294d910f5884d095158a250
Chahine Bendjeddou <chahine.bendjeddou99@gmail.com>
2023-06-16 06:54:30 +0100

    create the user avatar

---
+++ src/components/UserAvatar.tsx
@@ -0,0 +1,35 @@
+import { User } from 'next-auth'
+import { FC } from 'react'
+import { Avatar, AvatarFallback } from './ui/avatar'
+import { Divide } from 'lucide-react'
+import Image from 'next/image'
+import { Icons } from './Icons'
+import { AvatarProps } from '@radix-ui/react-avatar'
+
+interface UserAvatarProps extends AvatarProps {
+  user: Pick<User, 'name' | 'image'>
+}
+
+const UserAvatar: FC<UserAvatarProps> = ({ user, ...props }) => {
+  return (
+    <Avatar {...props}>
+      {user.image ? (
+        <div className="relative w-full h-full aspect-square">
+          <Image
+            fill
+            src={user.image}
+            alt="Profile picture"
+            referrerPolicy="no-referrer"
+          />
+        </div>
+      ) : (
+        <AvatarFallback>
+          <span>{user?.name}</span>
+          <Icons.user className="w-4 h-4" />
+        </AvatarFallback>
+      )}
+    </Avatar>
+  )
+}
+
+export default UserAvatar
