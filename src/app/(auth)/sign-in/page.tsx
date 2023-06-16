871c076ab4d4870dd1f65fc0c42d755ca52a34a4
Chahine Bendjeddou <chahine.bendjeddou99@gmail.com>
2023-06-16 10:09:43 +0100

    add a return icon to the go home btn in the sign in page

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,6 +1,7 @@
 import SignIn from '@/components/SignIn'
 import { buttonVariants } from '@/components/ui/Button'
 import { cn } from '@/lib/utils'
+import { ChevronLeft } from 'lucide-react'
 import Link from 'next/link'
 import { FC } from 'react'
 
@@ -15,7 +16,7 @@ const page: FC = () => {
             'self-start -mt-20'
           )}
         >
-          Home
+          <ChevronLeft className="w-4 h-4 mr-2" /> Home
         </Link>
         <SignIn />
       </div>
