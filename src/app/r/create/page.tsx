c14ee170463f1d5fdd7c70b6ab49ff990cd43e02
Chahine Bendjeddou <chahine.bendjeddou99@gmail.com>
2023-06-16 15:03:15 +0100

    finishing the create community page

--- src/app/r/create/page.tsx
+++ src/app/r/create/page.tsx
@@ -5,11 +5,14 @@ import { Input } from '@/components/ui/Input'
 import { useRouter } from 'next/navigation'
 import { FC, useState } from 'react'
 import { useMutation } from '@tanstack/react-query'
-import axios from 'axios'
+import axios, { AxiosError } from 'axios'
 import { CreateSubredditPayload } from '@/lib/validators/subreddit'
+import { toast } from '@/hooks/use-toast'
+import { useCustonToadt } from '@/hooks/use-custon-toast'
 const page: FC = () => {
   const [input, setInput] = useState<string>('')
   const router = useRouter()
+  const { loginToast } = useCustonToadt()
   const { mutate: createCommunity, isLoading } = useMutation({
     mutationFn: async () => {
       const payload: CreateSubredditPayload = {
@@ -17,6 +20,31 @@ const page: FC = () => {
       }
       const { data } = await axios.post('/api/subreddit', { payload })
       return data as string
+    },
+    onError: (err) => {
+      if (err instanceof AxiosError) {
+        if (err.response?.status === 409)
+          return toast({
+            title: 'Subreddit already exists.',
+            description: 'Please choose a different subreddit name.',
+            variant: 'destructive',
+          })
+        if (err.response?.status === 422)
+          return toast({
+            title: 'Invalid subreddit name.',
+            description: 'Please choose a name between 3 and 21 characters.',
+            variant: 'destructive',
+          })
+        if (err.response?.status === 401) return loginToast()
+      }
+      toast({
+        title: 'Something went wrong.',
+        description: 'Could not create a new subreddit',
+        variant: 'destructive',
+      })
+    },
+    onSuccess: (data) => {
+      router.push(`/r/${data}`)
     },
   })
   return (
