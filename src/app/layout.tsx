2dca6bf614fd7d6bd4be055bcfbe4bd0ec5e3eb7
Chahine Bendjeddou <chahine.bendjeddou99@gmail.com>
2023-06-16 09:59:24 +0100

    add the intercepted page to the layout app

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,8 +10,10 @@ export const metadata = {
 const inter = Inter({ subsets: ['latin'] })
 export default function RootLayout({
   children,
+  authModal,
 }: {
   children: React.ReactNode
+  authModal: React.ReactNode
 }) {
   return (
     <html
@@ -24,6 +26,7 @@ export default function RootLayout({
       <body className="min-h-screen pt-12 antialiased bg-slate-50">
         {/* @ts-expect-error server component */}
         <Navbar />
+        {authModal}
         <div className="container h-full pt-12 mx-auto max-w-7xl ">
           {children}
         </div>
