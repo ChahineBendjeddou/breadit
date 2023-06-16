4c21b86b547e80e672425c98dcd929525a02ede3
Chahine Bendjeddou <chahine.bendjeddou99@gmail.com>
2023-06-16 03:27:02 +0100

    creating thye navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,3 +1,17 @@
+import Link from 'next/link'
+import { Icons } from './Icons'
+
 export default function Navbar() {
-  return <div>Navbar</div>
+  return (
+    <nav className="fixed inset-x-0 top-0 z-10 py-2 border-b h-fit bg-zinc-100 border-zinc-300">
+      <div className="container flex items-center justify-between h-full gap-2 mx-auto max-w-7xl">
+        <Link href="/" className="flex items-center gap-2">
+          <Icons.logo className="w-8 h-8 sm:h-6 sm:w-6" />
+          <p className="hidden text-sm font-medium text-zinc-700 md:block">
+            Breadit
+          </p>
+        </Link>
+      </div>
+    </nav>
+  )
 }
