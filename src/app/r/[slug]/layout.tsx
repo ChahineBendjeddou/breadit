155879e405d76e4801b898e7ae8962a55456de36
Chahine Bendjeddou <chahine.bendjeddou99@gmail.com>
2023-06-17 06:47:57 +0100

    add a button to toggle subscription to a subreddit

--- src/app/r/[slug]/layout.tsx
+++ src/app/r/[slug]/layout.tsx
@@ -1,3 +1,4 @@
+import SubscribeLeaveToggle from '@/components/SubscribeLeaveToggle'
 import { getAuthSession } from '@/lib/auth'
 import { db } from '@/lib/db'
 import { format } from 'date-fns'
@@ -75,6 +76,10 @@ const layout = async ({ children, params: { slug } }: layoutProps) => {
                 <div className="flex justify-between py-3 gap-x-4">
                   <p className="text-gray-500">You created this community.</p>
                 </div>
+              ) : null}
+
+              {subreddit.creatorId !== session?.user.id ? (
+                <SubscribeLeaveToggle />
               ) : null}
             </dl>
           </div>
