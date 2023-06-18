ea916e1fdea9075d4056fd1ef80b16805a9538e9
Chahine Bendjeddou <chahine.bendjeddou99@gmail.com>
2023-06-18 19:22:29 +0100

    create the types for the rendered posts

---
+++ src/types/db.d.ts
@@ -0,0 +1,8 @@
+import { Post, Subreddit, User, Vote } from '@prisma/client'
+
+export type ExtendedPost = Post & {
+  author: User
+  votes: Vote[]
+  comments: Comment[]
+  subreddit: Subreddit
+}
