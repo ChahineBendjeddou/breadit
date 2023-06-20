98857938d8e5afa33fa5c2be383012629ea7e9b0
Chahine Bendjeddou <chahine.bendjeddou99@gmail.com>
2023-06-20 18:31:01 +0100

    create a comment component

---
+++ src/components/CreateComment.tsx
@@ -0,0 +1,31 @@
+'use client'
+import { FC, useState } from 'react'
+import { Button } from './ui/Button'
+import { Label } from './ui/Label'
+import { Textarea } from './ui/textarea'
+
+interface CreateCommentProps {}
+
+const CreateComment: FC<CreateCommentProps> = ({}) => {
+  const [input, setInput] = useState<string>('')
+
+  return (
+    <div className="grid w-full gap-1.5">
+      <Label htmlFor="comment">Your comment </Label>
+      <div className="mt-2">
+        <Textarea
+          id="comment"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          rows={1}
+          placeholder="What are your thoughts?"
+        />
+        <div className="flex justify-end mt-2">
+          <Button>Post</Button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default CreateComment
