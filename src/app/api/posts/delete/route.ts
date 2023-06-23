ac524a5f8a3a9562cf911921b4d834007f3d609b
Chahine Bendjeddou <chahine.bendjeddou99@gmail.com>
2023-06-23 21:22:13 +0100

    create the endpoint to handle the deletion of a post

---
+++ src/app/api/posts/delete/route.ts
@@ -0,0 +1,61 @@
+import { getAuthSession } from '@/lib/auth'
+import { db } from '@/lib/db'
+import { z } from 'zod'
+
+export async function DELETE(req: Request) {
+  try {
+    const session = await getAuthSession()
+    if (!session?.user) return new Response('Unauthorized', { status: 401 })
+
+    const url = new URL(req.url)
+    const postId = url.searchParams.get('postId')
+    if (!postId) return new Response('Invalid query', { status: 400 })
+    const commentsForVote = await db.comment.findMany({
+      where: {
+        postId,
+      },
+    })
+    for (const comment of commentsForVote) {
+      await db.commentVote.deleteMany({
+        where: {
+          commentId: comment.id,
+        },
+      })
+    }
+    const comments = await db.comment.findMany({
+      where: { postId: postId },
+      include: { Comments: true }, // Include nested comments (replies)
+    })
+
+    // Delete the nested comments (replies) first
+    const nestedCommentIds = comments
+      .flatMap((comment) => comment.Comments)
+      .map((comment) => comment.id)
+    await db.comment.deleteMany({
+      where: { id: { in: nestedCommentIds } },
+    })
+
+    // Delete the top-level comments associated with the post
+    const commentIds = comments.map((comment) => comment.id)
+    await db.comment.deleteMany({
+      where: { id: { in: commentIds } },
+    })
+    await db.vote.deleteMany({
+      where: {
+        postId,
+      },
+    })
+    await db.post.delete({
+      where: {
+        id: postId,
+      },
+    })
+    return new Response('Post deleted', { status: 200 })
+  } catch (error) {
+    if (error instanceof z.ZodError)
+      return new Response('Invalid request data passed', { status: 422 })
+    return new Response('Could not delete post' + error, {
+      status: 500,
+    })
+  }
+}
